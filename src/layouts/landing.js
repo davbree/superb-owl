4cd9c3b2bfc9d9d20486638535656bbd63f4ecd5
Stackbit <support@stackbit.com>
2021-01-05 07:48:59 +0000

    landing.js: changed

--- src/layouts/landing.js
+++ src/layouts/landing.js
@@ -7,6 +7,7 @@ export default class Landing extends React.Component {
     render() {
         return (
             <Layout {...this.props}>
+            ...
             {_.map(_.get(this.props, 'page.frontmatter.sections', null), (section, section_idx) => {
                 let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
                 let Component = components[component];
